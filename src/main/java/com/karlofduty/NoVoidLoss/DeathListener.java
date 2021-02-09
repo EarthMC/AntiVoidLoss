5fed0b9265aa4afb4699de637395f752e2688726
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-02-09 20:21:20 +0100

    Use setKeepInventory instead of complicating stuff.

--- src/main/java/com/karlofduty/NoVoidLoss/DeathListener.java
+++ src/main/java/com/karlofduty/NoVoidLoss/DeathListener.java
@@ -1,61 +1,21 @@
 package com.karlofduty.NoVoidLoss;
 
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.inventory.ItemStack;
-
-import java.util.Map;
 
 public class DeathListener implements Listener
 {
-    private NoVoidLoss plugin;
-
-    public DeathListener(final NoVoidLoss plugin)
-    {
-        this.plugin = plugin;
-    }
-
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onEvent(final PlayerDeathEvent event)
+    public void onPlayerDeath(final PlayerDeathEvent event)
     {
-        boolean restoreAllItems = false;
-        try {
-            restoreAllItems = event.getEntity().getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.VOID
-                           || event.getEntity().getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.SUFFOCATION;
-        }
-        catch (NullPointerException ignored) { }
-
-        final ItemEntry entry = new ItemEntry();
-        for (int i = 0; i < event.getEntity().getInventory().getSize(); ++i)
+        if (event.getEntity().getLocation().getY() < 50)
         {
-            final ItemStack stack = event.getEntity().getInventory().getItem(i);
-            if (stack != null && !stack.getType().equals(Material.AIR) && (stack.getType().equals(Material.PLAYER_HEAD) || restoreAllItems))
-            {
-                entry.getItems().put(i, stack);
-                event.getDrops().remove(stack);
-            }
-        }
-
-        if (!entry.getItems().isEmpty()) {
-            NoVoidLoss.items.put(event.getEntity().getUniqueId().toString(), entry);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onRespawn(final PlayerRespawnEvent event)
-    {
-        if (NoVoidLoss.items.containsKey(event.getPlayer().getUniqueId().toString()))
-        {
-            for (final Map.Entry<Integer, ItemStack> entry : NoVoidLoss.items.get(event.getPlayer().getUniqueId().toString()).getItems().entrySet())
-            {
-                event.getPlayer().getInventory().setItem(entry.getKey(), entry.getValue());
-            }
-            NoVoidLoss.items.remove(event.getPlayer().getUniqueId().toString());
+            event.getDrops().clear();
+            event.setDroppedExp(0);
+            event.setKeepInventory(true);
+            event.setKeepLevel(true);
         }
     }
 }
