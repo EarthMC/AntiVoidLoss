519e6c7bc517a44f44011f35dde0962515c7a8d6
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-02-10 00:11:09 +0100

    Protect against suffocation deaths

--- src/main/java/com/karlofduty/NoVoidLoss/DeathListener.java
+++ src/main/java/com/karlofduty/NoVoidLoss/DeathListener.java
@@ -4,13 +4,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 public class DeathListener implements Listener
 {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerDeath(final PlayerDeathEvent event)
     {
-        if (event.getEntity().getLocation().getY() < 50)
+        if (event.getEntity().getLocation().getY() < 50 || event.getEntity().getLastDamageCause().getCause() == DamageCause.SUFFOCATION)
         {
             event.getDrops().clear();
             event.setDroppedExp(0);
