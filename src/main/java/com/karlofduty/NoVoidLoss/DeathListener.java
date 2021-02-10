f732eac48d32070aebb0923329134512ce8aae43
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-02-10 01:11:22 +0100

    Add back player head protection

--- src/main/java/com/karlofduty/NoVoidLoss/DeathListener.java
+++ src/main/java/com/karlofduty/NoVoidLoss/DeathListener.java
@@ -1,10 +1,15 @@
 package com.karlofduty.NoVoidLoss;
 
+import java.util.Map.Entry;
+
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class DeathListener implements Listener
 {
@@ -17,6 +22,37 @@ public class DeathListener implements Listener
             event.setDroppedExp(0);
             event.setKeepInventory(true);
             event.setKeepLevel(true);
+        }
+        else if (event.getEntity().getInventory().contains(Material.PLAYER_HEAD) || event.getEntity().getInventory().getHelmet().getType().equals(Material.PLAYER_HEAD))
+        {
+            ItemEntry entry = new ItemEntry();
+            for (int i = 0; i < event.getEntity().getInventory().getSize(); i++)
+            {
+                ItemStack item = event.getEntity().getInventory().getItem(i);
+                if (item == null || item.getType() == Material.AIR)
+                    continue;
+                
+                if (item.getType().equals(Material.PLAYER_HEAD))
+                {
+                    event.getDrops().remove(item);
+                    entry.getItems().put(i, item);
+                }
+            }
+            if (!entry.getItems().isEmpty())
+                NoVoidLoss.playerSkulls.put(event.getEntity().getUniqueId(), entry);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerRespawn(PlayerRespawnEvent event)
+    {
+        if (NoVoidLoss.playerSkulls.containsKey(event.getPlayer().getUniqueId()))
+        {
+            for (Entry<Integer, ItemStack> item : NoVoidLoss.playerSkulls.get(event.getPlayer().getUniqueId()).getItems().entrySet())
+            {
+                event.getPlayer().getInventory().setItem(item.getKey(), item.getValue());
+            }
+            NoVoidLoss.playerSkulls.remove(event.getPlayer().getUniqueId());
         }
     }
 }
--- src/main/java/com/karlofduty/NoVoidLoss/NoVoidLoss.java
+++ src/main/java/com/karlofduty/NoVoidLoss/NoVoidLoss.java
@@ -1,10 +1,15 @@
 package com.karlofduty.NoVoidLoss;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoVoidLoss extends JavaPlugin
 {
+    public static Map<UUID, ItemEntry> playerSkulls = new HashMap<>();
     public void onEnable()
     {
         Bukkit.getPluginManager().registerEvents(new DeathListener(), this);
