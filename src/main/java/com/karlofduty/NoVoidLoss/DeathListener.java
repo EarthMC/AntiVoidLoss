db8d356fb4f637a6187667ce8bb3e0a73b542185
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-08-09 17:17:14 +0200

    Finally updating this

--- src/main/java/com/karlofduty/NoVoidLoss/DeathListener.java
+++ src/main/java/com/karlofduty/NoVoidLoss/DeathListener.java
@@ -1,14 +1,12 @@
 package com.karlofduty.NoVoidLoss;
 
-import java.util.Map.Entry;
-
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class DeathListener implements Listener
@@ -16,43 +14,24 @@ public class DeathListener implements Listener
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerDeath(final PlayerDeathEvent event)
     {
-        if (event.getEntity().getLocation().getY() < 100 || event.getEntity().getLastDamageCause().getCause() == DamageCause.SUFFOCATION)
+        Player player = event.getEntity();
+        if (player.getLastDamageCause() != null && (player.getLastDamageCause().getCause().equals(DamageCause.SUFFOCATION) || player.getLastDamageCause().getCause().equals(DamageCause.VOID)))
         {
             event.getDrops().clear();
             event.setDroppedExp(0);
             event.setKeepInventory(true);
             event.setKeepLevel(true);
         }
-        else if ((event.getEntity().getInventory().contains(Material.PLAYER_HEAD) || event.getEntity().getInventory().getHelmet().getType().equals(Material.PLAYER_HEAD)) && !event.getKeepInventory())
+        else
         {
-            ItemEntry entry = new ItemEntry();
-            for (int i = 0; i < event.getEntity().getInventory().getSize(); i++)
+            for (ItemStack item : player.getInventory())
             {
-                ItemStack item = event.getEntity().getInventory().getItem(i);
-                if (item == null || item.getType() == Material.AIR)
-                    continue;
-                
-                if (item.getType().equals(Material.PLAYER_HEAD))
+                if (item != null && item.getType().equals(Material.PLAYER_HEAD))
                 {
                     event.getDrops().remove(item);
-                    entry.getItems().put(i, item);
+                    event.getItemsToKeep().add(item);
                 }
             }
-            if (!entry.getItems().isEmpty())
-                NoVoidLoss.playerSkulls.put(event.getEntity().getUniqueId(), entry);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerRespawn(PlayerRespawnEvent event)
-    {
-        if (NoVoidLoss.playerSkulls.containsKey(event.getPlayer().getUniqueId()))
-        {
-            for (Entry<Integer, ItemStack> item : NoVoidLoss.playerSkulls.get(event.getPlayer().getUniqueId()).getItems().entrySet())
-            {
-                event.getPlayer().getInventory().setItem(item.getKey(), item.getValue());
-            }
-            NoVoidLoss.playerSkulls.remove(event.getPlayer().getUniqueId());
         }
     }
 }
